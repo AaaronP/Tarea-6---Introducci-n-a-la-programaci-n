6d9c536c322bcfaee7e0ad10f26a785045751e1a
Aaron <aaronperezwindows10@gmail.com>
2024-05-06 18:18:32 -0600

    ...

--- seccion1/sol_polin6.py
+++ seccion1/sol_polin6.py
@@ -1,17 +1,14 @@
-def encontrar_soluciones(coeficientes):
-    soluciones = []
-    for x in range(-1000, 1001):  # Ampliado el rango de valores de x
-        valor_polinomio = 0
-        for i, coeficiente in enumerate(coeficientes[::-1]):
-            print(coeficiente)
-            valor_polinomio += coeficiente * (x**i)
-        # Utilizar una tolerancia en lugar de igualdad exacta
-        if valor_polinomio == 0:
-            soluciones.append(x)
-    return soluciones
+def ceros(coeficientes):
+    invC = coeficientes[::-1]
+    pos_ceros = []
+    for x in range(-1000, 1001):
+        val = 0
+        for i, coeficiente in enumerate(invC):
+            val += coeficiente * (x**i)
+
+        if val == 0:
+            pos_ceros.append(x)
+    return pos_ceros
 
 
-# Ejemplo de uso
-coeficientes = [1, 3, -1, -3]
-soluciones = encontrar_soluciones(coeficientes)
-print(soluciones)
+print(ceros([3, 5, -2, 1, -7]))
